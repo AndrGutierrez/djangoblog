5889754438c98e384eb9ce9694d80cbeecfb0f38
Andrés <contact@andresgutierrezdev.me>
2022-02-17 01:30:01 -0400

    moving package.json to root

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -37,7 +37,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["app-djangoblog.herokuapp.com"]
+ALLOWED_HOSTS = [
+"localhost",
+"app-djangoblog.herokuapp.com"]
 
 # Application definition
 
